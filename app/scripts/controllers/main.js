64b75eadc2be54827dca48f1011fcfc175c07e82
Iain Andrew <iainbruceandrew@gmail.com>
2015-03-09 23:01:25 +0000

    Added ability to select both languages from dropdowns

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,21 +14,32 @@ angular.module('App')
     $scope.usernameSelected = false;
     $scope.stayAnon = false;
     $scope.anonUsername = 'anon' + Math.floor((Math.random()*90000)+10000); // generate random username for anons
+
+    var key = 'trnsl.1.1.20150309T154331Z.204e43f2acbf0788.022db5406add6b3a68a6f48e8e956adcb8a0df24';
+
+    // getLangs method for select elements
+    $http.get('https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=' + key + '&ui=en')
+      .success(function(data) {
+        $scope.langs = data.langs;
+        $scope.langsAbbrev = Object.keys($scope.langs); // abbreviated language names
+        $scope.selectedNativeLang = 'en';
+        $scope.selectedTranslateLang = 'fr';
+      })
+      .error(function(status) {
+        console.log(status);
+      });
+
     $scope.submit = function(e) {
 
-      var key = 'trnsl.1.1.20150309T154331Z.204e43f2acbf0788.022db5406add6b3a68a6f48e8e956adcb8a0df24';
-      //$http.get('https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=' + key + '&ui=en')
-      $http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + key + '&lang=en-ru&text=' + $scope.message)
+      // translate method
+      $http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + key + '&lang=' + $scope.selectedNativeLang + '-' + $scope.selectedTranslateLang + '&text=' + $scope.message)
         .success(function(data) {
           requestSuccess(data);
         })
-        .error(function(data, status) {
+        .error(function(status) {
           console.log(status);
         });
       function requestSuccess(data) {
-        // $scope.langs = data.langs;
-        // $scope.langsAbbrev = Object.keys($scope.langs);
-        // $scope.selectedNativeLang = 'en';
         $scope.translatedMessage = data.text[0];
 
         if ($scope.stayAnon) {
@@ -39,11 +50,13 @@ angular.module('App')
             mins = date.getMinutes(),
             secs = date.getSeconds();
         $scope.time = hours + ':' + mins + ':' + secs;
-        $scope.messages.$add({
+        $scope.messages.$add({ // save info to database
           user: $scope.name,
           anon: $scope.stayAnon,
           body: $scope.translatedMessage,
-          time: $scope.time
+          time: $scope.time,
+          from: $scope.selectedNativeLang,
+          to: $scope.selectedTranslateLang
         });
         $scope.usernameSelected = true;
         $scope.message = ""; // reset message input after submitted
